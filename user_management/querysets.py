ae8e0f4b423bc61a4b324ee0249d74aa49a704ac
majasemzadeh <ma.jasemzadeh@gmail.com>
2023-11-24 00:09:52 +0330

    Custome Queryset, the total_spending and the order count

--- user_management/querysets.py
+++ user_management/querysets.py
@@ -1,9 +1,15 @@
 from django.db.models import QuerySet
 
+from django.db.models import Sum, Count
+
 
 class CustomerQuerySet(QuerySet):
     def annotate_with_total_spending(self):
-        return self
+        return self.annotate(
+            total_spending=Sum('order__total_price')
+        )
 
     def annotate_with_order_count(self):
-        return self
+        return self.annotate(
+            order_count=Count('order')
+        )
