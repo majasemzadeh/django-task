a1bf3c8b8161c9115f6d745dcc23814da68ec119
majasemzadeh <ma.jasemzadeh@gmail.com>
2023-11-23 23:58:28 +0330

    fix the Order QuerySets for pass TestOrderQueryset

--- order/querysets.py
+++ order/querysets.py
@@ -1,15 +1,15 @@
-from django.db.models import QuerySet
+from django.db.models import QuerySet, Sum
 
 
 class OrderQuerySet(QuerySet):
     def by_customer(self, customer):
-        return self
+        return self.filter(customer=customer)
 
     def total_price(self):
-        return self
+        return self.aggregate(total_price=Sum('total_price')).get('total_price') or 0
 
     def total_price_by_customer(self, customer):
-        return self
+        return self.by_customer(customer).total_price()
 
     def submitted_in_date(self, date_value):
-        return self
+        return self.filter(date__date=date_value)
