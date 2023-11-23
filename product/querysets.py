3acbf7362c8fe75cdb9250ec30e7054a055062c1
majasemzadeh <ma.jasemzadeh@gmail.com>
2023-11-24 00:07:51 +0330

    fix the querset of product

--- product/querysets.py
+++ product/querysets.py
@@ -3,8 +3,8 @@ from django.db.models import QuerySet
 
 class ProductQuerySet(QuerySet):
     def needs_restock(self):
-        """returns a queryset of products that their stock is less than 10"""
-        return self
+        """Return a queryset of products with stock less than 10."""
+        return self.filter(stock__lt=10)
 
     def in_stock(self):
-        return self
+        return self.filter(stock__gt=10)
